298523e6bf1847823f0bb22869630d8ced943c0a
milxxfa <145160479+milxxfa@users.noreply.github.com>
2024-11-15 01:10:48 +0200

    complete

--- server/config/database.js
+++ server/config/database.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 sequelize.authenticate()
-    .then(() => console.log('Database connected...'))
+    .then(() => console.log('Database connected'))
     .catch(err => console.log('Error: ' + err));
 
 module.exports = sequelize;
